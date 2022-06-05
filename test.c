0467094b0f53bda04df220c099559cda6efe0fb6
Spring1515 <106728117+Spring1515@users.noreply.github.com>
2022-06-05 18:11:33 +0900

    お別れ

--- test.c
+++ test.c
@@ -3,4 +3,9 @@
 void main() {
     printf("Hello world\n");
     printf("Hello Sugimoto\n");
+<<<<<<< Updated upstream
+=======
+    printf("Hello Yoshida\n");
+    printf("Goodbye\n");
+>>>>>>> Stashed changes
 }
