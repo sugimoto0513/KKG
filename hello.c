190e01c0cb144366c8fe2c7bdad595593374319b
sugimoto0513 <k.sugimoto0513@gmail.com>
2022-06-05 18:55:47 +0900

    いいね

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@ void main() {
     printf("Hello world\n");
     printf("Hello Yoshida\n");
     printf("Hello Sugimoto\n");
-  　　　　printf("How are you ?\n");
+    printf("How are you ?\n");
     printf("I am fine\n");
+    printf("Oh well\n");
 }
